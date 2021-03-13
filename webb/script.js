a34193b467e51277f197381ebacd532709dff1e6
sepsep <safemontima@gmail.com>
2021-03-13 18:57:47 +0700

    fill

--- webb/script.js
+++ webb/script.js
@@ -1,12 +1,22 @@
 const x=5473;
+let inout={};
 // date picker range
   $(function() {
     $('input[name="daterange"]').daterangepicker({
-      opens: 'left'
+    opens: 'left'
     }, function(start, end, label) {
-      console.log(start.format('DD-MM-YYYY') + ' to ' + end.format('DD-MM-YYYY'));
-      cin = start.format('DD-MM-YYYY');
-      cout = end.format('DD-MM-YYYY');
+    console.log(start.format('DD-MM-YYYY') + ' to ' + end.format('DD-MM-YYYY'));
+    checkin = start.format('DD-MM-YYYY');
+    checkout = end.format('DD-MM-YYYY');
+    data={checkin,checkout};
+
+    // fetch("url" , {
+    // method:"POST",
+    // body: JSON.stringify(data)
+    // }).then(res => {
+    //     console.log("Request complete! response:",res);
+    // });
+
     //   document.getElementById("demo").innerHTML = start.format('DD-MM-YYYY') +" to "+ end.format('DD-MM-YYYY');
     });
   });
