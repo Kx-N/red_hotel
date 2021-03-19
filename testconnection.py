11a5c2c2ce938103407d98580fe03f9e8d505898
Kx-N <65390633+Kx-N@users.noreply.github.com>
2021-03-19 23:13:39 +0700

    Update testconnection.py

--- testconnection.py
+++ testconnection.py
@@ -222,6 +222,6 @@ def booking():
       #return str(book_id)
 
 if __name__ == "__main__":
-  app.run(host='192.168.1.38', port='5000', debug=True)
+  app.run(host='0.0.0.0', port='5000', debug=True)
 
 
